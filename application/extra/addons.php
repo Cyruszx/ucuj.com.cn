2571186c740031852a7a19ad8f3454d1165739b0
Lance-93 <glen92093@gmail.com>
2018-10-18 11:47:29 +0800

    upyun

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -4,9 +4,23 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
+    'addon_after_upgrade' => 
+    array (
+      0 => 'cms',
+    ),
+    'upload_config_init' => 
+    array (
+      0 => 'upyun',
+    ),
   ),
   'route' => 
   array (
+    '/cms$' => 'cms/index/index',
+    '/cms/c/[:diyname]' => 'cms/channel/index',
+    '/cms/t/[:name]' => 'cms/tags/index',
+    '/cms/a/[:diyname]' => 'cms/archives/index',
+    '/cms/p/[:diyname]' => 'cms/page/index',
+    '/cms/s' => 'cms/search/index',
     '/example$' => 'example/index/index',
     '/example/d/[:name]' => 'example/demo/index',
     '/example/d1/[:name]' => 'example/demo/demo1',
